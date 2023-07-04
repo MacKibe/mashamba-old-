a103ba8c8d352b2d61bc3eaec03ac13e843ee920
mackibe <ndichujames2030@gmail.com>
2023-07-04 10:00:21 +0300

    saving working without moving to the first document

--- v/code/mashamba.js
+++ v/code/mashamba.js
@@ -1,254 +1,237 @@
 //Access the server services by importing all the facilities that are in the
 //server module in the schema foler of our library
 import * as server from "../../../schema/v/code/server.js";
+//Access to Page class of our library
+import * as view from "../../../outlook/v/code/view.js";
 //
-//Declare the elements of interests
-//
-//The image element in in the panel one
-var first_page;
-//
-//This is the panel that represents the other pages of the document
-var other_pages;
-//
-// This is the next button element for displaying the next document
-var next_button;
-//
-// This is the previous button element for displaying the previous document
-var previous_button;
-//
-//The couner for documents being displayed
-var counter = 0;
-//
-//Declare the trsnscrption elements
-var input_Elements;
-//
-//Save the results of interrogating the database to an array of documnets
-var docs;
-//
-//Loading multipe titles that can move from one title to ythe other
-export async function load_mutiples_titles() {
+//Extend the page class with our own version, called mashamba
+export class mashamba extends view.page {
     //
-    //Initialize the variabls that represent html elemens in ou page, including settinup the event listeners
-    await initialize();
+    //Declare the elements of interests
     //
-    //Load the current title
-    load_title();
-}
-//
-//Initializing the element will
-async function initialize() {
+    //The image element in in the panel one
+    first_page;
     //
-    //intialize the page one panel
-    first_page = document.getElementById("first_page");
+    //This is the panel that represents the other pages of the document
+    other_pages;
     //
-    //intialize the other pages panel
-    other_pages = document.getElementById("other_pages");
+    //The couner for documents being displayed
+    counter = 0;
     //
-    //initialize the next button
-    next_button = document.getElementById("nxt_btn");
+    //The results of interrogating the database is an array of documents
+    docs;
     //
-    //initialize the previous button
-    previous_button = document.getElementById("previous_btn");
     //
-    // Attach an event listener for moving to the document
-    next_button.onclick = () => move_next();
-    //
-    // Attach an event listener for moving to the document
-    previous_button.onclick = () => move_previous();
-    //
-    // Attach an event listener for moving to the document
-    document.getElementById("save_data_btn").onclick = () => save_data();
-    //
-    //Initialize the transcription elements
-    input_Elements = document.getElementById("type");
-    //
-    //Read the data from the masjambe database and save it ith the documents
-    //array
-    docs = await server.exec("database", ["mutall_mashamba", false], "get_sql_data", ["/mashamba/v/code/mashamba.sql", "file"]);
-}
-//
-// Load the current document tothehome page depending
-async function load_title() {
-    //
-    // Clear all the 3 panels, viz., first_page, other_pages and transcription
-    clear_panels();
-    //
-    // Get the pages of the given current document number
-    const pages = JSON.parse(docs[counter].pages);
-    //
-    // Create the first page, including its image
-    create_first_page(pages[0]);
-    //
-    // Fill the transcription panel
-    for (const key of [
-        "document",
-        "title_no",
-        "category",
-        "area",
-        "owner",
-        "regno",
-    ])
-        fill_transcriptions(key);
-    //
-    // Create and show the other_pages panel
-    for (let i = 1; i < pages.length; i++)
-        create_other_page(pages[i]);
-}
-function create_other_page(page) {
-    //
-    // Create an image element for this page
-    const image = document.createElement("img");
-    //
-    // Set the source of the image to the URL of the page
-    image.src = `http://localhost${page.url}`;
-    //
-    // Attach the image element to the other-pages div element
-    other_pages.appendChild(image);
-}
-//clear all the 3 panels
-function clear_panels() {
-    //
-    // Clear the first page
-    first_page.innerHTML = "";
-    //
-    // Clear the other_pages panels
-    other_pages.innerHTML = "";
-    //
-    // Clear all the inputs of the transcription panel, by looping over all
-    // the keys of a document, except the pages key
-    /*
-      document:string,
-          pages:string,
-          title_no:string,
-          category:string,
-          area:number,
-          owner:string,
-          regno:string
-      */
-    for (const key of [
-        "document",
-        "title_no",
-        "category",
-        "area",
-        "owner",
-        "regno",
-    ]) {
+    constructor() {
+        super();
         //
-        // Skip the pages key (because it is a special key)
+        //intialize the page one panel
+        this.first_page = document.getElementById("first_page");
+        //
+        //intialize the other pages panel
+        this.other_pages = document.getElementById("other_pages");
+        //
+        // Attach an event listener for moving to the document
+        document.getElementById("nxt_btn").onclick = () => this.move_next();
+        //
+        // Attach an event listener for moving to the document
+        document.getElementById("previous_btn").onclick = () => this.move_previous();
+        //
+        // Attach an event listener for saving the transcriptions
+        document.getElementById("save_data_btn").onclick = () => this.save_data();
+    }
+    //Replace the show pannels method with our own version
+    async show_panels() {
+        //
+        //Load documents
+        this.docs = await server.exec("database", ["mutall_mashamba", false], "get_sql_data", ["/mashamba/v/code/mashamba.sql", "file"]);
+        //
+        //Load the current title
+        this.load_title();
+    }
+    // this will help in moving to next document
+    move_next() {
+        //
+        // Increate the counter by 1
+        this.counter++;
+        // Load tthe titles using the new counter
+        this.load_title();
+    }
+    //
+    // this will help in moving to next document
+    move_previous() {
+        //
+        // Increate the counter by 1
+        this.counter--;
+        // Load tthe titles using the new counter
+        this.load_title();
+    }
+    //
+    // Load the current document tothehome page depending
+    async load_title() {
+        //
+        // Clear all the 3 panels, viz., first_page, other_pages and transcription
+        this.clear_panels();
+        //
+        // Get the pages of the given current document number
+        const pages = JSON.parse(this.docs[this.counter].pages);
+        //
+        // Create the first page, including its image
+        this.create_first_page(pages[0]);
+        //
+        // Fill the transcription panel
+        for (const key of [
+            "document",
+            "title_no",
+            "category",
+            "area",
+            "owner",
+            "regno",
+        ])
+            this.fill_transcriptions(key);
+        //
+        // Create and show the other_pages panel
+        for (let i = 1; i < pages.length; i++)
+            this.create_other_page(pages[i]);
+    }
+    //
+    // Set the url of the first image of the pages
+    create_first_page(page) {
+        //
+        // Get url of of the first page
+        const url = page.url;
+        //
+        // Create the first page image
+        const image1 = document.createElement("img");
+        //
+        // Attach the image to page1
+        this.first_page.appendChild(image1);
+        //
+        // Set the url of the page
+        image1.src = `http://localhost${url}`;
+    }
+    create_other_page(page) {
+        //
+        // Create an image element for this page
+        const image = document.createElement("img");
+        //
+        // Set the source of the image to the URL of the page
+        image.src = `http://localhost${page.url}`;
+        //
+        // Attach the image element to the other-pages div element
+        this.other_pages.appendChild(image);
+    }
+    //clear all the 3 panels
+    clear_panels() {
+        //
+        // Clear the first page
+        this.first_page.innerHTML = "";
+        //
+        // Clear the other_pages panels
+        this.other_pages.innerHTML = "";
+        //
+        // Clear all the inputs of the transcription panel, by looping over all
+        // the keys of a document, except the pages key
+        /*
+          document:string,
+              pages:string,
+              title_no:string,
+              category:string,
+              area:number,
+              owner:string,
+              regno:string
+          */
+        for (const key of [
+            "document",
+            "title_no",
+            "category",
+            "area",
+            "owner",
+            "regno",
+        ]) {
+            //
+            // Skip the pages key (because it is a special key)
+            if (key === "pages")
+                continue;
+            //
+            // Get the named element
+            const element = document.getElementById(key);
+            //
+            // Se its value to empty
+            element.value = "";
+        }
+    }
+    // Fill the transcriptions, by transferring the values from from the global
+    // array, data array to
+    // the transciption panel
+    fill_transcriptions(key) {
+        //
+        //Skip the pages key (because it is a special key)
         if (key === "pages")
-            continue;
+            return;
         //
-        // Get the named element
+        //Get the named element
         const element = document.getElementById(key);
         //
-        // Se its value to empty
-        element.value = "";
+        //Get the value that maches the key
+        const value = this.docs[this.counter][key];
+        //
+        //Set the element vale only if the value is not null
+        if (value !== null)
+            element.value = String(value);
     }
-}
-//
-// Set the url of the first image of the pages
-function create_first_page(page) {
     //
-    // Get url of of the first page
-    const url = page.url;
-    //
-    // Create the first page image
-    const image1 = document.createElement("img");
-    //
-    // Attach the image to page1
-    first_page.appendChild(image1);
-    //
-    // Set the url of the page
-    image1.src = `http://localhost${url}`;
-}
-//
-// this will help in moving to next document
-function move_next() {
-    //
-    // Increate the counter by 1
-    counter++;
-    // Load tthe titles using the new counter
-    load_title();
-}
-//
-// this will help in moving to next document
-function move_previous() {
-    //
-    // Increate the counter by 1
-    counter--;
-    // Load tthe titles using the new counter
-    load_title();
-}
-// Fill the transcriptions, by transferring the values from from the global
-// array, data array to
-// the transciption panel
-function fill_transcriptions(key) {
-    //
-    //Skip the pages key (because it is a special key)
-    if (key === "pages")
-        return;
-    //
-    //Get the named element
-    const element = document.getElementById(key);
-    //
-    //Get the value that maches the key
-    const value = docs[counter][key];
-    //
-    //Set the element vale only if the value is not null
-    if (value !== null)
-        element.value = String(value);
-}
-//
-// Get the data from the input elements and send and save them to various 
-// tables in the mutall_mashamba database 
-async function save_data() {
-    //
-    //Collect the data to save, as layouts
-    //
-    //Get the ids of the html elements that hp;d the data
-    const ids = {
-        document: ['document', 'id'],
-        title_no: ['title', 'id'],
-        category: ['category', 'name'],
-        area: ['document', 'area'],
-        owner: ['document', 'person'],
-        regno: ['document', 'regno']
-    };
-    //
-    //The elements will now bw mapped with layouts
-    const layouts = Object.keys(ids).map(k => {
+    // Get the data from the input elements and send and save them to various 
+    // tables in the mutall_mashamba database 
+    async save_data() {
         //
-        //Coerce k into of of the document keys
-        const key = k;
+        //Collect the data to save, as layouts
         //
-        //Get the values of the elements
-        const value = document.getElementById(key).value;
+        //Get the ids of the html elements that hp;d the data
+        const ids = {
+            document: ['document', 'id'],
+            title_no: ['title', 'id'],
+            category: ['category', 'name'],
+            area: ['document', 'area'],
+            owner: ['document', 'person'],
+            regno: ['document', 'regno']
+        };
         //
-        //Show the entity name where the data will be saved in the database 
-        const ename = ids[key][0];
+        //The elements will now be mapped to their layouts
+        const layouts = Object.keys(ids).map(k => {
+            //
+            //Coerce k into of of the document keys
+            const key = k;
+            //
+            //Get the values of the elements
+            const value = document.getElementById(key).value;
+            //
+            //Show the entity name where the data will be saved in the database 
+            const ename = ids[key][0];
+            //
+            //Show which column in the database the value will be saved
+            const cname = ids[key][1];
+            //
+            //Get the values ready for saving
+            return [value, ename, cname];
+        });
         //
-        //Show which column in the database the value will be saved
-        const cname = ids[key][1];
+        //Use questionnaire to save the data and get the results
+        const result = await server.exec(
         //
-        //Get the values ready for saving
-        return [value, ename, cname];
-    });
-    //
-    //Use questionnaire to save the data and get the results
-    const result = await server.exec(
-    //
-    //The name of the PHP class to use is questionnaire
-    "questionnaire", 
-    //
-    //The constructor parameter of questionnare is one: database name
-    ['mutall_mashamba'], 
-    //
-    //The name of the questionnare method to use is the common lodig system
-    'load_common', 
-    //
-    //The mandory parameter of the load commom method is one: layput
-    [layouts]);
-    //
-    //Report the result
-    alert(result);
+        //The name of the PHP class to use is questionnaire
+        "questionnaire", 
+        //
+        //The constructor parameter of questionnare is one: database name
+        ['mutall_mashamba'], 
+        //
+        //The name of the questionnare method to use is the common lodig system
+        'load_common', 
+        //
+        //The mandory parameter of the load commom method is one: layput
+        [layouts]);
+        //
+        //Report the result.
+        alert(result);
+    }
 }
